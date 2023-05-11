a2807f592bcbde1532e26ffcf30bd286d15a810d
Christian Panter <pantercp@outlook.com>
2023-05-11 13:50:09 +0100

    Added intersection observer for scroll effect on nav

--- app.js
+++ app.js
@@ -16,3 +16,26 @@ burgerBtn.addEventListener('click', () => {
     menuOpen = false;
   }
 });
+
+const header = document.querySelector("#header");
+const sectionOne = document.querySelector(".hero-section");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
