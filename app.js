a444dff49142ce137346f36479eed3a628cefdac
Christian Panter <pantercp@outlook.com>
2023-05-11 12:47:00 +0100

    Styled nav bar and added drop down for mobile responsiveness

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const burgerBtn = document.querySelector('.row-burger');
 const dropMenu = document.querySelector('.drop-menu');
+const navTitle = document.querySelector('.logo-title');
 
 let menuOpen = false;
 burgerBtn.addEventListener('click', () => {
   if (!menuOpen) {
     burgerBtn.classList.add('open');
     dropMenu.classList.add('open');
+    navTitle.classList.add('open');
     menuOpen = true;
   } else {
     burgerBtn.classList.remove('open');
     dropMenu.classList.remove('open');
+    navTitle.classList.remove('open');
     menuOpen = false;
   }
 });
