57da9a84d4b256b888aaf10b38df35003ae24a04
Christian Panter <pantercp@outlook.com>
2023-05-14 11:02:27 +0100

    Added intersection observer with styling for fade-in and slide-in

--- app.js
+++ app.js
@@ -20,11 +20,14 @@ burgerBtn.addEventListener('click', () => {
 const header = document.querySelector("#header");
 const sectionOne = document.querySelector(".hero-section");
 
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
 const sectionOneOptions = {
   rootMargin: "-200px 0px 0px 0px"
 };
 
-const sectionOneObserver = new IntersectionObserver(function(
+const sectionOneObserver = new IntersectionObserver(function (
   entries,
   sectionOneObserver
 ) {
@@ -36,6 +39,32 @@ const sectionOneObserver = new IntersectionObserver(function(
     }
   });
 },
-sectionOneOptions);
+  sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 1,
+  // rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+  (function (entries, appearOnScroll) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    })
+  },
+    appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+})
